3d51d05a868a298b8db3636d911db7e198585445
Sudip Panja <panja370@gmail.com>
2022-01-14 01:39:23 +0530

    Added : ResponseDTO class for generating the response of various Http request which contains a string message

--- src/main/java/com/bridgelabz/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/dto/ResponseDTO.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseDTO {
+    private HttpStatus httpStatus;
+    private String message;
 }
