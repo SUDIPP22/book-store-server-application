9477ade905ad8e66489a83bcc9f4ea11c31fb07a
Sudip Panja <panja370@gmail.com>
2022-01-14 01:38:11 +0530

    Added : BookDetailsDTO class to invoke the data from user end

--- src/main/java/com/bridgelabz/dto/BookDetailsDTO.java
+++ src/main/java/com/bridgelabz/dto/BookDetailsDTO.java
@@ -4,16 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookDetailsDTO {
-    private  String bookName;
+    @NotNull(message = "Book name should not be empty")
+    private String bookName;
+
+    @NotNull(message = "Author name should not be empty")
+    @Pattern(regexp = "^[A-Z][a-z]{2,}[ ][A-z][a-z]{2,}$", message = "Name is invalid")
     private String bookAuthorName;
+
+    @NotNull(message = "Image should not be empty")
     private String bookImage;
-    private String bookPrice;
+
+    @NotNull(message = "Price should not be empty")
+    private double bookPrice;
+
     private boolean bookAvailablity;
+
+    @NotNull(message = "Description should not be empty")
+    @Size(message = "Description should be within 2000 words", max = 2000)
     private String bookDescription;
+
     private double bookRatings;
+
+    @NotNull(message = "Reviews should not be empty")
     private String bookReviews;
 }
