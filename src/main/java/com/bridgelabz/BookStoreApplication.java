802112a15c93fade13f4167977088dffc815d598
Sudip Panja <panja370@gmail.com>
2022-01-14 01:36:58 +0530

    Added : Enabling encrypting properties for Jasypt encryption in book store application

--- src/main/java/com/bridgelabz/BookStoreApplication.java
+++ src/main/java/com/bridgelabz/BookStoreApplication.java
@@ -1,9 +1,11 @@
 package com.bridgelabz;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
